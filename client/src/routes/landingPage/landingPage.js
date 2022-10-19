7d74a072d98e9a79ed99b01ddde69fed82ab8048
Chris HS <chris.hollidaysmith@travelpartners.com.au>
2022-10-19 12:29:50 +1000

    Landing page styling

--- client/src/routes/landingPage/landingPage.js
+++ client/src/routes/landingPage/landingPage.js
@@ -6,10 +6,15 @@ export const LandingPage = () => {
     return (
         <div id="content">
             <div id="content-left">
-                <p id="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p id="text-heading">"I owe you. Yewomi."</p>
+                <p id="text-hero">Making sharing expenses, easy!</p>
+                <aside id="text-aside">Remove all the unwanted stress of trying to split a bill 10 ways, or keeping track of <i>who owes what</i>. With Yewomi, you can track your shared expenses among friends, family & housemates - across a variety of different groups, trips, shopping sprees or a big night out! And the best part - it's completely FREE! ...<span>Yewomi!</span> 😉...</aside>
                 <Link to='/signup'><button id='sign-up'>Sign up</button></Link>
             </div>
-            <img id="content-right" src='#' alt='image'/>
+            <div id="content-right">
+                <img  src='https://images.pexels.com/photos/1209843/pexels-photo-1209843.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='image'/>
+                <div id="overlay"></div>
+            </div>
         </div>
     )
 }
