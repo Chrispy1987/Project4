0f2bb6dcff2a67ee6e2d4affc80e8d21fa3ad30b
Chris HS <chris.hollidaysmith@travelpartners.com.au>
2022-10-26 01:12:25 +1000

    Structured 'View Group' view, including code blocks to generate which user paid and who owes money.

--- client/src/routes/viewGroup/ViewGroup.js
+++ client/src/routes/viewGroup/ViewGroup.js
@@ -34,48 +34,86 @@ export const ViewGroup = (props) => {
         console.log(data)
         setGroupInfo(data)
     }
-
+    console.log(groupInfo)
     return (
-        <>
+        <>  
             {groupInfo && 
             <>
-                <ul id='REFERENCE_ONLY_DELETE_LATER'>
+                {/* <ul id='REFERENCE_ONLY_DELETE_LATER'>
                     <li>GROUP OWNER: {helper.capitaliseFirstLetter(groupInfo.owner)}</li>
                     <li>GROUP NAME: {groupInfo.name}</li>
                     <li>STATUS: {groupInfo.settled ? 'This group has been settled' : 'Not settled'}</li>
-                </ul>
-                <h2>{groupInfo.name}</h2>
-                {groupInfo && <button onClick={()=>{}}> + New Expense</button>}
-                {groupInfo ? groupInfo.expenses.map((expense) => {
+                </ul> */}
+                <h2 id='group-name'>{groupInfo.name}</h2>
+                {groupInfo.expenses.length === 0 && 
+                    <>
+                        <p><i>"The secret of getting ahead is getting started" ~ Mark Twain</i></p>
+                    </>
+                }
+                <button className='new-expense-button' onClick={()=>{}}> + New Expense</button>
+                {groupInfo && groupInfo.expenses.map((expense) => {
+                    const lender = helper.capitaliseFirstLetter(expense.lender)
+                    const amountPaid = helper.convertCentsToDollars(expense.amount);
+                    const description = helper.capitaliseFirstLetter(expense.description);
+                    
+                    let userPortion;
+                    let isLender;
+                    if (expense.lender_id === props.session) {
+                        isLender = true;
+                        userPortion = helper.convertCentsToDollars(expense.transactions.map(transaction => transaction.amount).reduce((partial, amount) => partial + amount, 0));
+                    } else {
+                        isLender = false;
+                        userPortion = helper.convertCentsToDollars(expense.transactions.filter(transaction => transaction.user_id === props.session).map(get => get.amount));
+                    }
+
+
                     return (
                         // <Expenses />   Set up new componenet to handle below
-                        <div className='expense-line'>
-                            <b>---Expense lines here---</b>
-                            <ul id='REFERENCE_ONLY_DELETE_LATER'>
+                        <>
+                            <div className='expense-line'>
+                                <div className='container-date'>
+                                    {/* <p>{expense.date}</p> */}
+                                    <p className='date-day'>12</p>
+                                    <p className='date-month'>OCT</p>
+                                </div>
+                                <div className='container-icon'>
+                                    <img className='icon' src='https://cdn-icons-png.flaticon.com/512/8100/8100406.png'/>
+                                </div>
+                                <div className='container-description'>
+                                    <p className='description'>{description}</p>
+                                    <p className='amount-paid'>{isLender ? 'You paid' : `${lender} paid`} {amountPaid}</p>
+                                </div>
+                                <div className='container-lent'>
+                                    <p className={isLender ? 'user-lender green' : 'user-lender red'}>{isLender ? 'You lent' : 'You borrowed'}</p>
+                                    <p className={isLender ? 'user-portion green' : 'user-portion red'}>{userPortion}</p>
+                                </div>
+                            </div>
+                           
+
+
+                            {/* <b>----------Expense lines here----------</b> */}
+                            {/* <ul id='REFERENCE_ONLY_DELETE_LATER'>
+                                <li>LENDER: {expense.lender} - userId {expense.lender_id}</li>
                                 <li>AMOUNT: {expense.amount}</li>
                                 <li>DATE: {expense.date}</li>
                                 <li>ICON: {expense.icon}</li>
                                 <li>DESCRIPTION: {expense.description}</li>
-                            </ul>
+                            </ul> */}
                             {expense.transactions.map(transaction => {
                                 return (
                                     <div>
-                                        <b>---Transaction lines here---</b>
-                                        <ul id='REFERENCE_ONLY_DELETE_LATER'>
-                                            <li>BORROWER {transaction.borrower}</li>
-                                            <li>AMOUNT ALLOCATED {transaction.amount}</li>
-                                        </ul>
+                                        {/* <b>**Transaction lines here**</b> */}
+                                        {/* on expense click -  */}
+                                        {/* <ul id='REFERENCE_ONLY_DELETE_LATER'>
+                                            <li>BORROWER {transaction.borrower} - userId {transaction.user_id}</li>
+                                            <li>OWES {transaction.amount}</li>
+                                        </ul> */}
                                     </div>
                                 )
                             })}
-                        </div>
+                        </>
                     )
-                })
-                : <div>
-                    <p>No expenses have been recorded for this group</p>
-                    <button onClick={()=>{}}>ADD EXPENSE</button>
-                    </div>
-                }
+                })}
             </>
             }
         </>
