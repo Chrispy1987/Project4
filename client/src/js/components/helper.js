ef62798a5d58075ad409a21d2ee3ac0de98fd3fa
Chris HS <chris.hollidaysmith@travelpartners.com.au>
2022-10-27 01:01:33 +1000

    Added functions to helper to extract Day and Month from a date

--- client/src/js/components/helper.js
+++ client/src/js/components/helper.js
@@ -1,4 +1,13 @@
 export const helper = {
     capitaliseFirstLetter: word => word.charAt(0).toUpperCase() + word.slice(1),
-    convertCentsToDollars: number => `$${(number/100).toFixed(2)}`
+    convertCentsToDollars: number => `$${(number/100).toFixed(2)}`,
+    getDay: (date) => {
+        const d = new Date(date);
+        return d.getDate()
+    },
+    getMonth: (date) => {
+        const month = ["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"];
+        const d = new Date(date);
+        return month[d.getMonth()]
+    }
 };
