a3848c32c9c9f6eca933fecb1c46b27dfc5e7d49
Chris HS <chris.hollidaysmith@travelpartners.com.au>
2022-10-29 01:57:19 +1000

    descoped security question feature

--- database/seed.sql
+++ database/seed.sql
@@ -4,11 +4,11 @@
 -- Note: passwords for users are same as usernames
 
 TRUNCATE users restart identity cascade;
-INSERT INTO users(username, email, password, security_qn, security_ans, is_admin) VALUES
-('chris', 'chris@ga.com', '$2b$10$wc..uujGo3b3ceXnN80tzOYaWuPXQKu.0GoEWdc2jKVi47g91xDAi', 1, 'test', 't'),
-('sid', 'sid@ga.com', '$2b$10$dhxz25GjwVgV2YkgJxBK8eWPxoSjOJywSdnRXfbz78WYXvlj7el6S', 2,'test', 't'),
-('dave', 'dave@ga.com', '$2b$10$ng..DJ0BE9VdUvUTk3.dqunsplnc673tHnT9I/g3tY4aD7LgWzCnK', 3,'test', 't'),
-('sam', 'sam@ga.com', '$2b$10$kfi/.jZLiYM6hBWEagFvgeaN/IKSJFFdNuop0IhTbyGHYOMGZxY6G', 4,'test', 'f');
+INSERT INTO users(username, email, password, is_admin) VALUES
+('chris', 'chris@ga.com', '$2b$10$wc..uujGo3b3ceXnN80tzOYaWuPXQKu.0GoEWdc2jKVi47g91xDAi', 't'),
+('sid', 'sid@ga.com', '$2b$10$dhxz25GjwVgV2YkgJxBK8eWPxoSjOJywSdnRXfbz78WYXvlj7el6S', 't'),
+('dave', 'dave@ga.com', '$2b$10$ng..DJ0BE9VdUvUTk3.dqunsplnc673tHnT9I/g3tY4aD7LgWzCnK','t'),
+('sam', 'sam@ga.com', '$2b$10$kfi/.jZLiYM6hBWEagFvgeaN/IKSJFFdNuop0IhTbyGHYOMGZxY6G', 'f');
 
 
 TRUNCATE groups restart identity cascade;
